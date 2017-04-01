f14987f169af00f789b34f2736d5ae38b3124cef
arn-pannatier <arn.pannatier@gmail.com>
2017-04-01 18:11:00 +0200

    Adaptation to Swift - With Colorisation

--- Solitaire.playground/Contents.swift
+++ Solitaire.playground/Contents.swift
@@ -4,35 +4,24 @@ import UIKit
 import PlaygroundSupport
 import Darwin
 
-let containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0))
+
+
+var ColorField = [
+    "darkBlue": UIColor(red:0.17, green:0.24, blue:0.31, alpha:1.0),
+    "lightBlue": UIColor(red:0.20, green:0.60, blue:0.86, alpha:1.0),
+    "lightGray": UIColor(red:0.93, green:0.94, blue:0.95, alpha:1.0),
+    "green" : UIColor(red:0.18, green:0.80, blue:0.44, alpha:1.0),
+    "red" : UIColor(red:0.91, green:0.30, blue:0.24, alpha:1.0),
+    "yellow" : UIColor(red:0.95, green:0.77, blue:0.06, alpha:1.0)
+]
+
+
+var containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 400, height: 400))
+containerView.backgroundColor = ColorField["lightGray"]
+
 PlaygroundPage.current.liveView = containerView;
 
-class Dot {
-    var stat: Bool = true
-    var dotAjd: Direction?
-    var id : Int?
-    var action : [Dot]?
-    var view : Circle = Circle(pos: CGPoint(),dia: 0.0)
-    
-    init(){
-        
-    
-    }
-}
 
-class Circle : UIView {
-    init(pos: CGPoint, dia: CGFloat){
-        super.init(frame: CGRect(x: pos.x, y: pos.y, width: dia, height: dia))
-        self.layer.cornerRadius = 25.0
-        let startingColor = UIColor(red: (253.0/255.0), green: (159.0/255.0), blue: (47.0/255.0), alpha: 1.0)
-        self.backgroundColor = startingColor
-        containerView.addSubview(self);
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
 class Direction {
     var up: Int?
     var left: Int?
@@ -45,13 +34,55 @@ class Direction {
         self.left = l
         self.right = r
     }
+    subscript(name: String) -> Dot? {
+        switch name {
+        case "up":
+            return (self.up == nil) ? nil : mapDot[self.up!]
+        case "left":
+            return (self.left == nil) ? nil : mapDot[self.left!]
+        case "right":
+            return (self.right == nil) ? nil :mapDot[self.right!]
+        case "down":
+            return (self.down == nil) ? nil : mapDot[self.down!]
+        default:
+            return Dot()
+        }
+    }
 }
 
 
-var ColorField = [
-    "bleuFonce": UIColor(red:0.17, green:0.24, blue:0.31, alpha:1.0)
-]
 
+class Circle : UIView {
+    init(pos: CGPoint, dia: CGFloat){
+        super.init(frame: CGRect(x: pos.x, y: pos.y, width: dia, height: dia))
+        self.layer.cornerRadius = dia/2+1
+        self.backgroundColor = ColorField["lightBlue"]
+        containerView.addSubview(self);
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class Dot {
+    var stat: Bool
+    var dotAjd: Direction
+    var id : Int
+    var action : [String]
+    var view : Circle
+    
+    init(){
+        self.stat = true
+        self.dotAjd = Direction(u: nil,l: nil,r: nil,d: nil)
+        self.id = 0
+        self.action = []
+        self.view = Circle(pos: CGPoint(),dia: 0.0)
+        
+    
+    }
+
+}
 
 
 var mapDot : [Dot] = []
@@ -61,18 +92,10 @@ var completementBloque = false
 var numberOfCase = 33
 
 
-
-func initialize() {
-    createField();
-    //play(container);
-}
 func createField(){
     let w: CGFloat = containerView.frame.size.width;
     let rdot: CGFloat =  w/15.0;
     let milieuDot: CGFloat = w/2.0;
-    //dot.style.width = rdot+"px";
-    //dot.style.height = rdot+"px";
-
     let centralDots = 21
     let coteGauche = 27
     let coteDots = 6
@@ -80,12 +103,12 @@ func createField(){
     let centerNumber = 10
 
     
-    for i in 1..<numberOfCase {
-            mapDot[i] = Dot()
+    for _ in 0..<numberOfCase {
+        mapDot.append(Dot())
     }
         
         
-    for i in 1..<centralDots {
+    for i in 0..<centralDots {
         let posX: CGFloat = milieuDot-rdot*(2.5-CGFloat((i%3)*2))
         let posY: CGFloat = rdot*(1+floor(CGFloat(i)/3.0)*2)
         
@@ -96,7 +119,7 @@ func createField(){
             //Centre
         if(i==centerNumber){
             mapDot[i].stat = false;
-            mapDot[i].view.backgroundColor = ColorField["bleuFonce"];
+            mapDot[i].view.backgroundColor = ColorField["darkBlue"];
         }
             
         
@@ -105,8 +128,8 @@ func createField(){
         }
         
         
-    for i in 1..<coteDots {
-        for j in 1..<cotes {
+    for i in 0..<coteDots {
+        for j in 0..<cotes {
             let k=centralDots+i*2+j;
             
             
@@ -119,13 +142,14 @@ func createField(){
                 posX = rdot+CGFloat(j)*rdot*2;
             }
             let pos = CGPoint(x: posX, y : posY )
+
             
             mapDot[k].id = k;
-            mapDot[i].view = Circle(pos: pos,dia: rdot)
+            mapDot[k].view = Circle(pos: pos,dia: rdot)
         }
     }
         
-    for i in 1..<numberOfCase{
+    for i in 0..<numberOfCase{
             
             if(i<centralDots){
                 if(i%3==0){
@@ -202,15 +226,9 @@ func createField(){
         }
 }
 
-initialize()
+/*
 
- /*
-    play : function(container){
-        GA.algo();
-        
-        
-    },
-    joue_coup : function(d1,dir){
+func joue_coup(d1,dir){
         var couleur1 = d1.elem;
         var couleur2 = d1.dotAjd[dir].dotAjd[dir].elem;
         d1.stat = 0;
@@ -220,28 +238,31 @@ initialize()
         dotPlay = new Array();
         Solitaire.colorise();
         
-        couleur1.style.backgroundColor = "#2ecc71";
-        couleur2.style.backgroundColor = "#e74c3c";
+        couleur1.style.backgroundColor = ColorField["Green"];
+        couleur2.style.backgroundColor = ColorField["Red"];
         
-    },
-    colorise : function(){
-        Solitaire.actionZero();
-        compt = 0;
+}
+ */
+func colorise(){
+        //actionZero();
+        var compt = 0;
         
-        for(i=0; i<33; i++){
-            if(mapDot[i].stat == 0){
-                mapDot[i].elem.style.backgroundColor = "#2c3e50";
-            }else if(outils.to_play(mapDot[i])){
-                compt++;
-                mapDot[i].elem.style.backgroundColor = "#f1c40f";
+    for i in 0..<numberOfCase {
+            if(!mapDot[i].stat){
+                mapDot[i].view.backgroundColor = ColorField["darkBlue"];
+            }else if(to_play(dot: mapDot[i])){
+                compt += 1;
+                mapDot[i].view.backgroundColor = ColorField["yellow"];
             }else {
-                compt++;
-                mapDot[i].elem.style.backgroundColor = "#3498db";
+                compt += 1;
+                mapDot[i].view.backgroundColor = ColorField["lightBlue"];
                 
             }
             
         }
-    },
+    }
+
+/*
     playArray : function(arr){
         var k = 0;
         Solitaire.remiseAZero();
@@ -366,29 +387,28 @@ initialize()
     },
     
 };
-
-var outils = {
-    to_play : function(tab){
-        var play = false;
-        var dir = new Array("up", "left", "right", "down");
+*/
+func to_play(dot: Dot) -> Bool {
+    var play = false;
+    let dir = ["up", "left", "right", "down"];
         
-        localementBloque = true;
-        for(j=0; j<dir.length; j++){
+    var localementBloque = true;
+    for d in dir {
             // Playable
-            if(outils.test_exist(tab, dir[j]) && tab.dotAjd[dir[j]].stat ==1 && tab.dotAjd[dir[j]].dotAjd[dir[j]].stat == 0){
+            if(test_exist(t: dot, dir: d) && dot.dotAjd[d]!.stat && !dot.dotAjd[d]!.dotAjd[d]!.stat){
                 play = true;
-                tab.action.push(dir[j]);
-                dotPlay.push(tab);
+                dot.action.append(d);
+                dotPlay.append(dot);
             }
             
             // Completely isolated		
-            if(tab.dotAjd[dir[j]] != nil){
-                var cur = tab.dotAjd[dir[j]];
-                if(cur.stat == 0){
-                    for (var k = 0; k < dir.length; k++) {
-                        var secondNeighbourg = cur.dotAjd[dir[k]];
-                        if(secondNeighbourg != nil && secondNeighbourg != tab){
-                            if(secondNeighbourg.stat != 0){
+            if(dot.dotAjd[d] != nil){
+                let cur = dot.dotAjd[d]!;
+                if(!cur.stat){
+                    for secondDir in dir {
+                        let secondNeighbourg = cur.dotAjd[secondDir];
+                        if(secondNeighbourg != nil && secondNeighbourg!.id != dot.id){
+                            if(secondNeighbourg!.stat){
                                 localementBloque = false;
                             }
                         }
@@ -402,15 +422,25 @@ var outils = {
         if(localementBloque){
             completementBloque = true;
         }
-        
-        
-        
         return play;
-    },
-    test_exist : function(t, dir){
-        return (t.dotAjd[dir] != nil && t.dotAjd[dir].dotAjd[dir] != nil) ? true : false;
-        
+}
+func test_exist(t : Dot, dir: String) -> Bool {
+    if(t.dotAjd[dir] != nil){
+        if t.dotAjd[dir]!.dotAjd[dir] != nil {
+            return true;
+        }
     }
-};
- 
- */
+
+    return false;
+}
+
+
+
+func initialize() {
+    createField()
+    colorise()
+
+    
+    
+}
+initialize()
